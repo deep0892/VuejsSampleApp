7b0c1a18b59332451c9c4a36a7f2c570028a0cde
Deepankar <deepankar080892@gmail.com>
2020-05-09 13:08:57 +0200

    commit

---
+++ Wonderful_Quotes/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteDeleted="deleteQuote"
+    ></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import QuotesGrid from "./components/QuotesGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+export default {
+  data: function () {
+    return {
+      quotes: ["This is frist quote"],
+      maxQuotes: 10,
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete a quotes first!");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    },
+  },
+  components: {
+    appQuoteGrid: QuotesGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header,
+  },
+};
+</script>
+
+<style></style>
---
+++ Wonderful_Quotes/src/components/QuotesGrid.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row">
+    <app-quote
+      v-for="(quote, index) in quotes"
+      :key="index"
+      @click.native="deleteQuote(index)"
+      >{{ quote }}</app-quote
+    >
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+export default {
+  props: ["quotes"],
+  data: function () {
+    return {};
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    },
+  },
+  components: {
+    appQuote: Quote,
+  },
+};
+</script>
+
+<style scoped></style>
