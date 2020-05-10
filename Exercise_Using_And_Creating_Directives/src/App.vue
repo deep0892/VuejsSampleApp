51d4f2a13d30b7f2d259a9aaa61ab14c8f416968
Deepankar <deepankar080892@gmail.com>
2020-05-10 03:28:30 +0200

    Exercise & Solution Using_And_Creating_Directives

---
+++ Exercise_Using_And_Creating_Directives/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives Exercise</h1>
+        <!-- Exercise -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+        <button v-myon:click="click">
+          This is a custom directive to execute a function
+        </button>
+        <hr />
+        <div
+          style="width: 100px; height: 100px; background-color: lightgreen;"
+          v-myon:mouseenter="mouseEnter"
+          v-myon:mouseleave="mouseLeave"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    myon: {
+      bind(el, binding, vnode) {
+        const type = binding.arg;
+        const fn = binding.value;
+        el.addEventListener(type, fn);
+      },
+    },
+  },
+  methods: {
+    click() {
+      alert("Customon click event triggered!");
+    },
+    mouseEnter() {
+      alert("mouseEnter!!!");
+    },
+    mouseLeave() {
+      alert("mouseLeave!!!");
+    },
+  },
+};
+</script>
+
+<style></style>
