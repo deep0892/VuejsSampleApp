268df60af687c1639e696aba725eeb547b5ea0c8
Deepankar <deepankar080892@gmail.com>
2020-06-20 21:54:43 +0200

    Understanding template urls

--- Connecting_to_Server_via_HTTP_Using_Vue_resource/src/App.vue
+++ Connecting_to_Server_via_HTTP_Using_Vue_resource/src/App.vue
@@ -13,6 +13,8 @@
         </div>
         <button class="btn btn-primary" @click="submit">Submit</button>
         <hr />
+        <input type="text" class="form-control" v-model="node" />
+        <br />
         <button class="btn btn-primary" @click="fetchData">Get Data</button>
         <br />
         <ul class="list-group">
@@ -35,6 +37,7 @@ export default {
       },
       users: [],
       resource: {},
+      node: 'data',
     };
   },
   methods: {
@@ -49,8 +52,20 @@ export default {
       this.resource.saveAlt(this.user);
     },
     fetchData() {
-      this.$http
-        .get('data.json')
+      //   this.$http
+      //     .get('data.json')
+      //     .then((response) => {
+      //       return response.json();
+      //     })
+      //     .then((data) => {
+      //       const resultArray = [];
+      //       for (let key in data) {
+      //         resultArray.push(data[key]);
+      //       }
+      //       this.users = resultArray;
+      //     });
+      this.resource
+        .getData({ node: this.node })
         .then((response) => {
           return response.json();
         })
@@ -66,8 +81,9 @@ export default {
   created() {
     const customActions = {
       saveAlt: { method: 'POST', url: 'alternative.json' },
+      getData: { method: 'GET' },
     };
-    this.resource = this.$resource('data.json', {}, customActions);
+    this.resource = this.$resource('{node}.json', {}, customActions);
   },
 };
 </script>
