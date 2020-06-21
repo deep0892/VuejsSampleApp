a8342076b4fad51106eb1c44856c65e9df82ac77
Deepankar <deepankar080892@gmail.com>
2020-06-21 13:12:04 +0200

    Making router Link more dynamics,named routes

--- Routing/src/components/user/UserDetail.vue
+++ Routing/src/components/user/UserDetail.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <h3>Some User Details {{ $route.params.id }}</h3>
+    <h3>Some User Details</h3>
+    <p>User loaded has ID {{ $route.params.id }}</p>
+    <router-link
+      tag="button"
+      :to="{ name: 'userEdit', params: $route.params.id }"
+      class="btn btn-primary"
+      >Edit User</router-link
+    >
   </div>
 </template>
 
