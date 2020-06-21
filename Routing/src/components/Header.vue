4de43021722a9441fcd8bb75bb5a5fa07b683ee2
Deepankar <deepankar080892@gmail.com>
2020-06-21 12:25:03 +0200

    Reacting to change in route parameters

--- Routing/src/components/Header.vue
+++ Routing/src/components/Header.vue
@@ -3,8 +3,14 @@
     <router-link to="/" tag="li" active-class="active" exact
       ><a>Home</a></router-link
     >
-    <router-link to="/user/10" tag="li" active-class="active"
-      ><a>User</a></router-link
+    <router-link to="/user/1" tag="li" active-class="active"
+      ><a>User1</a></router-link
+    >
+    <router-link to="/user/2" tag="li" active-class="active"
+      ><a>User2</a></router-link
+    >
+    <router-link to="/user/:id" tag="li" active-class="active" props="true"
+      ><a>User2</a></router-link
     >
   </ul>
 </template>
--- Routing/src/components/user/User.vue
+++ Routing/src/components/user/User.vue
@@ -14,9 +14,15 @@ export default {
       id: this.$route.params.id,
     };
   },
+  watch: {
+    $route(to, from) {
+      this.id = to.params.id;
+    },
+  },
   methods: {
     navigateToHome() {
-      this.$router.push({ path: '/' });
+      this.$router.push('/');
+      //   this.$router.push({ path: '/' });
     },
   },
 };
