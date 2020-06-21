c0b06a43f2dcf00de7dce0baa9cbb0770120b17b
Deepankar <deepankar080892@gmail.com>
2020-06-21 13:58:19 +0200

    Passing the hash fragment

--- Routing/src/App.vue
+++ Routing/src/App.vue
@@ -5,7 +5,9 @@
         <h1>Routing</h1>
         <hr />
         <router-view name="header-top"></router-view>
-        <router-view></router-view>
+        <transition name="slide">
+          <router-view></router-view>
+        </transition>
         <router-view name="header-bottom"></router-view>
       </div>
     </div>
@@ -13,12 +15,12 @@
 </template>
 
 <script>
-// import Header from './components/Header.vue';
-// export default {
-//   components: {
-//     appHeader: Header,
-//   },
-// };
+import Header from './components/Header.vue';
+export default {
+  components: {
+    appHeader: Header,
+  },
+};
 </script>
 
 <style></style>
