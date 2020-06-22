ee8ecc782cdbcae41e098d761a40bb58a9580845
Deepankar <deepankar080892@gmail.com>
2020-06-22 14:29:34 +0200

    Loading routes lazily

--- Routing/src/routes.js
+++ Routing/src/routes.js
@@ -1,9 +1,31 @@
-import User from './components/user/User.vue';
-import UserStart from './components/user/UserStart.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
 import Home from './components/Home.vue';
 import Header from './components/Header.vue';
+
+// Lazy loading of components
+const User = (resolve) => {
+    require.ensure(['./components/user/User.vue'], () => {
+        resolve(require('./components/user/User.vue'));
+    });
+};
+
+// Also group lazy loading of component to be imported together.
+const UserStart = (resolve) => {
+    require.ensure(['./components/user/UserStart.vue'], () => {
+        resolve(require('./components/user/UserStart.vue'));
+    }, 'userstart');
+};
+
+const UserDetail = (resolve) => {
+    require.ensure(['./components/user/UserDetail.vue'], () => {
+        resolve(require('./components/user/UserDetail.vue'));
+    }, 'userstart');
+};
+
+const UserEdit = (resolve) => {
+    require.ensure(['./components/user/UserEdit.vue'], () => {
+        resolve(require('./components/user/UserEdit.vue'));
+    });
+};
 
 export const routes = [{
         path: '',
